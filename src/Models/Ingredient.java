33d8eb379e545b8c56129510c137e3ae812e5164
Iodine <sjdsoundworks@gmail.com>
2019-11-26 15:14:24 +0000

    started ingredients class

---
+++ src/Models/Ingredient.java
@@ -0,0 +1,14 @@
+package Models;
+
+public class Ingredient {
+
+    public String name;
+    public String description;
+    public double ABV;
+
+    public Ingredient(String name, String description, double ABV) {
+        this.name = name;
+        this.description = description;
+        this.ABV = ABV;
+    }
+}
