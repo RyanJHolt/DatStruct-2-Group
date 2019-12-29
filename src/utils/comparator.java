2fd285ed3887a32adea9de9006af4d6fb5b00311
RyanJHolt <44439777+RyanJHolt@users.noreply.github.com>
2019-12-29 20:18:07 +0000

    refactor of sorting code

--- src/utils/comparator.java
+++ src/utils/comparator.java
@@ -5,48 +5,45 @@ import Models.Ingredient;
 
 public class comparator {
 
-    public static boolean moreThanABV(Ingredient ing1,Ingredient ing2){
-        double ABV1 = ing1.getABV();
-        double ABV2 = ing2.getABV();
-        return ABV1>=ABV2;
+    public static boolean moreThanABV(linkedList.linkedNode obj1,linkedList.linkedNode obj2){
+        double ABV1 = 0;
+        double ABV2 = 0;
+        if (obj1.getContents() == Drink.class && obj2.getContents() == Drink.class){
+            Drink comp1 = (Drink) obj1.getContents();
+            Drink comp2 = (Drink) obj2.getContents();
+            ABV1 = comp1.getABV();
+            ABV2 = comp2.getABV();
+        } else if (obj1.getContents() == Ingredient.class && obj2.getContents() == Ingredient.class) {
+            Ingredient comp1 = (Ingredient) obj1.getContents();
+            Ingredient comp2 = (Ingredient) obj2.getContents();
+            ABV1 = comp1.getABV();
+            ABV2 = comp2.getABV();
+        }
+        return ABV1 >= ABV2;
     }
 
-    public static boolean moreThanABV(Drink dri1, Drink dri2){
-        double ABV1 = dri1.getABV();
-        double ABV2 = dri2.getABV();
-        return ABV1>=ABV2;
-    }
-
-    public static boolean Alphabetical(Ingredient Obj1,Ingredient Obj2){
+    public static boolean Alphabetical(linkedList.linkedNode obj1,linkedList.linkedNode obj2){
         int i = 0;
         char firstObj = 0;
         char secObj = 0;
-        boolean compare;
+        String objName1 = "";
+        String objName2 = "";
+        if (obj1.getContents() == Drink.class && obj2.getContents() == Drink.class){
+            Drink comp1 = (Drink) obj1.getContents();
+            Drink comp2 = (Drink) obj2.getContents();
+            objName1 = comp1.getName();
+            objName2 = comp2.getName();
+        } else if (obj1.getContents() == Ingredient.class && obj2.getContents() == Ingredient.class) {
+            Ingredient comp1 = (Ingredient) obj1.getContents();
+            Ingredient comp2 = (Ingredient) obj2.getContents();
+            objName1 = comp1.getName();
+            objName2 = comp2.getName();
+        }
         do {
-            firstObj = Obj1.getName().toUpperCase().charAt(i);
-            secObj = Obj2.getName().toUpperCase().charAt(i);
+            firstObj = objName1.toUpperCase().charAt(i);
+            secObj = objName2.toUpperCase().charAt(i);
         } while (firstObj == secObj);
-        return firstObj<secObj;
-    }
-
-    public static boolean Alphabetical(Drink Obj1,Drink Obj2){
-        int i = 0;
-        char firstObj = 0;
-        char secObj = 0;
-        boolean compare;
-        do {
-            firstObj = Obj1.getName().toUpperCase().charAt(i);
-            secObj = Obj2.getName().toUpperCase().charAt(i);
-        } while (firstObj == secObj);
-        return firstObj<secObj;
-    }
-
-    public static double getABV(Drink drink){
-        return drink.getABV();
-    }
-
-    public static double getABV(Ingredient ingredient){
-        return ingredient.getABV();
+        return firstObj <= secObj;
     }
 
 }
--- src/utils/shellSort.java
+++ src/utils/shellSort.java
@@ -1,124 +1,33 @@
 package utils;
 
-import Models.Ingredient;
-
 public class shellSort {
-    linkedList gaps = new linkedList();
 
-    //search function that the rest are based on kept for reference purposes
-    /*public void shellSort(linkedList a) {
-        if (gaps.getElement(1) == null) {
-            gaps.addElementH(1);
-            gaps.addElementH(3);
-            gaps.addElementH(5);
-        } //creates the gaps that the algorithm sorts within
-        for (int g = 0; g < gaps.size(); g++) {
-            int gap = (int) gaps.getElement(g).getContents(); //sets gap to one of the gaps created earlier
-            for (int e = gap; e < a.size(); e++) { //sets e to gap than reduces it each iteration
-                int elem = (int) a.getElement(e).getContents(), i; //initialises i as an int and sets elem to a specific element, in conjunction with the loop
-                for (i = e; i >= gap && (int) a.getElement(i - gap).getContents() > elem; i -= gap) {
-                    a.updateElement(i, a.getElement(i - gap));
-                    a.getElement(i).setContents(elem);
+    public void sortABV(linkedList a){
+        int listSize = a.size();
+        for (int gap = listSize/2; gap > 0; gap /= 2 ){ //creates large gap than reduces it after every pass
+            for (int i = gap; i < listSize; i += 1){ // Do a gapped insertion sort for this gap size. keeps adding one more element until the entire array is gap sorted
+                int j;
+                linkedList.linkedNode temp = a.getElement(i);
+                for (j = i; j >= gap && comparator.moreThanABV(a.getElement(j-gap),temp); j -= gap) {
+                    a.updateElement(j, a.getElement(j - gap));
                 }
+                a.updateElement(j, temp.getContents());
             }
-        }
-    } */
-
-    //Will be refactored once I have hashing done
-
-    //sort Drinks by ABV%
-    public void sortABVD(linkedList a) {
-        int interval = 1;
-        int outer;
-        int inner;
-        double insertValue;
-
-        while (interval < a.size()) {
-            interval = (interval * 3) + 1;
-        }
-        while (interval > 0) {
-            for (outer = interval; outer < a.size(); outer++) {
-                inner = outer;
-                insertValue = a.getDrink(outer).getABV();
-                while (inner > interval - 1 && (a.getDrink(inner - interval).getABV()) >= insertValue) {
-                    a.updateElement(inner, a.getElement(inner - interval));
-                    inner -= interval;
-                }
-                a.updateElement(inner, a.getElement(outer));
-            }
-            interval = (interval - 1) / 3;
         }
     }
 
-    //sort Ingredient by ABV%
-    public void sortABVI(linkedList a) {
-        int interval = 1;
-        int outer;
-        int inner;
-        double insertValue;
-
-        while (interval < a.size()) {
-            interval = (interval * 3) + 1;
-        }
-        while (interval > 0) {
-            for (outer = interval; outer < a.size(); outer++) {
-                inner = outer;
-                insertValue = (a.getIngredient(outer).getABV());
-                while (inner > interval - 1 && (a.getIngredient(inner - interval).getABV()) >= insertValue) {
-                    a.updateElement(inner, a.getElement(inner - interval));
-                    inner -= interval;
+    public void sortAlpha(linkedList a){
+        int listSize = a.size();
+        for (int gap = listSize/2; gap > 0; gap /= 2 ){
+            for (int i = gap; i < listSize; i += 1){
+                //int temp = a.getElement(i).getQuantity();
+                int j;
+                linkedList.linkedNode temp = a.getElement(i);
+                for (j = i; j >= gap && comparator.Alphabetical(a.getElement(j-gap),temp); j -= gap) {
+                    a.updateElement(j, a.getElement(j - gap));
                 }
-                a.updateElement(inner, a.getElement(outer));
+                a.updateElement(j, temp.getContents());
             }
-            interval = (interval - 1) / 3;
         }
     }
-
-    //Sort drinks alphabetically by name
-    public void sortAlphaD(linkedList a) {
-        int interval = 1;
-        int outer;
-        int inner = 0;
-        double insertValue;
-
-        while (interval < a.size()) {
-            interval = (interval * 3) + 1;
-        }
-        while (interval > 0) {
-            for (outer = interval; outer < a.size(); outer++) {
-                inner = outer;
-                insertValue = (a.getDrink(outer).getABV());
-                while((inner > interval - 1) && comparator.Alphabetical(a.getDrink(inner-interval),a.getDrink(outer)))
-                    a.updateElement(inner, a.getElement(inner - interval));
-                    inner -= interval;
-                }
-                a.updateElement(inner, a.getElement(outer));
-            }
-            interval = (interval - 1) / 3;
-        }
-
-        //Sort Ingredients alphabetically by name
-    public void sortAlphaI(linkedList a) {
-        int interval = 1;
-        int outer;
-        int inner = 0;
-        double insertValue;
-
-        while (interval < a.size()) {
-            interval = (interval * 3) + 1;
-        }
-        while (interval > 0) {
-            for (outer = interval; outer < a.size(); outer++) {
-                inner = outer;
-                insertValue = (a.getDrink(outer).getABV());
-                while((inner > interval - 1) && comparator.Alphabetical(a.getIngredient(inner-interval),a.getIngredient(outer)))
-                    a.updateElement(inner, a.getElement(inner - interval));
-                inner -= interval;
-            }
-            a.updateElement(inner, a.getElement(outer));
-        }
-        interval = (interval - 1) / 3;
-    }
-
-
 }
