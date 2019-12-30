c78859eaeea0812eef0d223f3b60c3ae834dff19
RyanJHolt <44439777+RyanJHolt@users.noreply.github.com>
2019-12-30 18:58:48 +0000

    Converted hashMap to use Array
    Turns out using a linkedList for hashing is less efficiant than not hashing and according to Georgia we can use Arrays for hashing so this is that now

--- src/utils/hashMap.java
+++ src/utils/hashMap.java
@@ -1,81 +1,78 @@
 package utils;
 
+@SuppressWarnings({"unchecked", "rawtypes"})
 public class hashMap<k,c> {
     //used to store hash chains
-    private linkedList hashList;
+    private hashNode[] hashArray;
     //Total capacity of hashList
     private int capacity;
     //Amount stored in hashList
     private int size;
 
     public hashMap() {
-        this.hashList = new linkedList<>();
         this.capacity = 10;
+        this.hashArray = new hashNode[capacity];
         this.size = 0;
 
         // Create empty chains
         for (int i = 0; i < capacity; i++)
-            hashList.addElementT(null);
+            hashArray[i] = null;
     }
 
     public int getSize() {
         return size;
     }
 
-    public boolean isEmpty(){
-        return getSize() == 0;
-    }
-
     // This implements hash function to find index for a key
     public int getHashIndex(k key){
-        //magic
+        //magic also know as an inbuilt function of the Object class that determines object hashcode
         int hashCode = key.hashCode();
         //set index
-        int index = hashCode % capacity;
-        return index;
+        return hashCode % capacity;
     }
 
     //remove a key
-    public c removeKey(k key){
+    public void removeKey(k key){
         //use hash function to find the index of the given key
         int hashIndex = getHashIndex(key);
         //get head of the chain
-        hashNode head = (hashNode) hashList.getElement(hashIndex).getContents();
+        hashNode<k,c> head = hashArray[hashIndex];
 
         //search the chain for the key
-        hashNode prev = null;
+        hashNode<k,c> prev = null;
         while (head != null){
             //if key found stops looping
             if (head.key.equals(key))
-                break;//else advances through the list
+                break;
+            //else advances through the list
             prev = head;
             head = head.next;
-            }
+        }
         //if key not found
         if (head == null)
-            return null;
-        //reduce size
+            return;
+        //else reduce size
         size--;
         //when found replace key with previous key if it exists
         if (prev != null)
             prev.next = head.next;
         else //if there is no previous key replace it with the next key
-            hashList.updateElement(hashIndex,head.next);
+            hashArray[hashIndex] = head.next;
 
-        //return the contents of the deleted key for testing purposes
-        return (c) head.content;
+        /*return the contents of the deleted key for testing purposes
+        return (c) head.getContent();*/
     }
 
-    public c getContent(k key) {
+    public c get(k key){
         int hashIndex = getHashIndex(key);
         //get head of the chain for given key
-        hashNode head = (hashNode) hashList.getElement(hashIndex).getContents();
+        hashNode<k,c> head = hashArray[hashIndex];
 
         //search the chain for the key
         while (head != null) {
             //if key found stops looping and returns content associated with key
-            if (head.key.equals(key))
-                return (c) head.content;
+            if (head.getKey().equals(key))
+                return head.getContent();
             //else advances through the list
             head = head.next;
         }
@@ -86,11 +83,11 @@ public class hashMap<k,c> {
     public void add(k key, c content){
         int hashIndex = getHashIndex(key);
         //get head of the chain for given key
-        hashNode head = (hashNode) hashList.getElement(hashIndex).getContents();
+        hashNode head = hashArray[hashIndex];
         //search the chain for the key
         while (head != null) {
             //if key found stops looping and sets content associated with key to content given
-            if (head.key.equals(key)){
+            if (head.getKey().equals(key)) {
                 head.content = content;
                 return;
             } //else advances through the chain
@@ -98,44 +95,48 @@ public class hashMap<k,c> {
         }
         //insert key into chain
         size++;
-        head = (hashNode) hashList.getElement(hashIndex).getContents();
-        hashNode newNode = new hashNode(key, content);
+        head = hashArray[hashIndex];
+        hashNode<k, c> newNode = new hashNode(key, content);
         newNode.next = head;
-        hashList.updateElement(hashIndex,newNode);
+        hashArray[hashIndex] = newNode;
         //increases size if space runs out
-        if ((1.0*getSize())/capacity >= 0.7){
-            linkedList temp = hashList;
-            hashList = new linkedList();
+        if ((1.0 * getSize()) / capacity >= 0.7) {
+            hashNode<k, c>[] temp = hashArray;
             capacity = 2 * capacity;
+            hashArray = new hashNode[capacity];
             size = 0;
             //makes a new list bigger than the previous one and transfers everything in
             for (int i = 0; i < capacity; i++) {
-                hashList.addElementT(null);
+                hashArray[i] = (null);
             }
-            linkedList.linkedNode tempNode = temp.getHead();
-                while (tempNode != null)
-                {
-                    hashNode headNode = (hashNode) tempNode.getContents();
-                    while (headNode != null) {
-                        add((k) headNode.key,(c) headNode.content);
-                        headNode = headNode.next;
-                    }
+            for (hashNode<k, c> tempNode : temp) {
+                while (tempNode != null){
+                    add(tempNode.getKey(), tempNode.getContent());
                     tempNode = tempNode.next;
                 }
             }
         }
+    }
 
 
 
 
-    public class hashNode<k,c>{
+    public static class hashNode<k,c>{
         k key;
         c content;
         hashNode next;
 
-        public hashNode(k key, c content) {
+        public hashNode(k key, c content){
             this.key = key;
             this.content = content;
+        }
+
+        public k getKey(){
+            return key;
+        }
+
+        public c getContent(){
+            return content;
         }
     }
 }
